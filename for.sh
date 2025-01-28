baad404b54b822535aec7ebedae47946256620de
Akash Chandran <67275861+AkashChand6n@users.noreply.github.com>
2025-01-28 16:58:40 +0530

    sequence in for loop

--- for.sh
+++ for.sh
@@ -1,4 +1,32 @@
-#!/bin/bash
+#!/bin/sh
+
+for I in 1 2 3 4 5 #this is used to iterate over the values
+do
+    echo "$I"
+done
+
+echo "******************************************"
+
+for I in {1..5} #this is used to generate a sequence of numbers
+do
+    echo "$I"
+done
+echo "******************************************"
+
+for I in $(seq 1 5) #seq is used to generate a sequence of numbers
+do
+    echo "$I"
+done
+
+echo "******************************************"
+
+for (( I=1; I<=5; I++ )) #I++ is used to increment the value of I by 1
+do 
+    echo "$I"
+done
+
+echo "******************************************"
+
 fruits=("apple" "banana" "cherry" "mango") #array is created here
 for fruits in "${fruits[@]}"; #array is used here synatx is ${array[@]}
 do
