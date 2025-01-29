546949806a6a7209c8fe499829f919d84d0f2e79
Akash Chandran <67275861+AkashChand6n@users.noreply.github.com>
2025-01-29 16:17:53 +0530

    Write a script to convert a string to uppercase.

---
+++ upper_case.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Write a script to convert a string to uppercase.
+#29-01-2025
+
+echo "Enter the string"
+read -r string
+echo "The string in uppercase is ${string^^}" #^^ is used to convert the string to uppercase
