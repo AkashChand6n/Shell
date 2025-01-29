9cd583709bf7714c75e4a442f711202c89ad84dd
Akash Chandran <67275861+AkashChand6n@users.noreply.github.com>
2025-01-29 12:18:27 +0530

    backup the file

---
+++ backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#backup of the file and save in the backup folder
+#29-01-2025
+
+function backup{
+    echo "enter the file name"
+    read -r file
+    if [ -f $file ]
+    then
+        echo "file exists"
+        mkdir backup
+        cp $file ./backup/$file
+        echo "backup of the file is done"
+    else
+        echo "file does not exist"
+    fi
+}
+
+backup
